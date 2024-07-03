33292c856783f167def20a971b7f2809936741f8
pattananNP <phattanun19@hotmail.com>
2024-07-03 19:08:57 +0700

    chore: Remove unused VSCode configuration files and update db.c

--- src/db.c
+++ src/db.c
@@ -9,49 +9,43 @@
 #define TABLE_MAX_PAGES 100
 #define PAGE_SIZE 4096
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        printf("Must supply a database filename.\n");
-        exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Must supply a database filename.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  InputBuffer *input_buffer = new_input_buffer();
+  print_welcome_message();
+
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_commnand(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
 
-    char *filename = argv[1];
-
-    InputBuffer *input_buffer = new_input_buffer();
-    print_welcome_message();
-
-    while (true)
-    {
-        print_prompt();
-        read_input(input_buffer);
-
-        if (input_buffer->buffer[0] == '.')
-        {
-            switch (do_meta_commnand(input_buffer))
-            {
-            case (META_COMMAND_SUCCESS):
-                continue;
-
-            case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                printf("Unrecognized command '%s'\n", input_buffer->buffer);
-                continue;
-            }
-        }
-
-        Statement statement;
-        switch (prepare_statement(input_buffer, &statement))
-        {
-        case (PREPARE_SUCCESS):
-            break;
-        case (PREPARE_UNRECOGNIZED_STATEMENT):
-            printf("Unrecognized keyword at start of '%s'.\n",
-                   input_buffer->buffer);
-            continue;
-        }
-
-        excute_statement(&statement);
-        printf("Executed.\n");
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n",
+               input_buffer->buffer);
+        continue;
     }
+
+    excute_statement(&statement);
+    printf("Executed.\n");
+  }
 }
--- src/executor.c
+++ src/executor.c
@@ -1,28 +1,28 @@
-#include "repl.h"
 #include "executor.h"
+
 #include <stdio.h>
 
-void excute_statement(Statement *statement)
-{
-    switch (statement->type)
-    {
+#include "repl.h"
+
+void excute_statement(Statement *statement) {
+  switch (statement->type) {
     case (STATEMENT_INSERT):
-        printf("This is where insert statement \n");
-        break;
+      printf("This is where insert statement \n");
+      break;
 
     case (STATEMENT_SELECT):
-        printf("This is where select \n");
-        break;
+      printf("This is where select \n");
+      break;
 
     case (STATEMENT_DELETE):
-        printf("This is where delete statement \n");
-        break;
+      printf("This is where delete statement \n");
+      break;
 
     case (STATEMENT_UPDATE):
-        printf("This us where update statement \n");
-        break;
+      printf("This us where update statement \n");
+      break;
     case (STATEMENT_UNRECOGNIZED):
-        printf("This is where unrecognized statement \n");
-        break;
-    }
+      printf("This is where unrecognized statement \n");
+      break;
+  }
 }
